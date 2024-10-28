277cd546441b441198f587dc5a43a1253a658ff7
RemDelaporteMathurin <rdelaportemathurin@gmail.com>
2024-10-28 13:00:50 -0400

    more complex empty mesh

--- case_1_simple/mesh.py
+++ case_1_simple/mesh.py
@@ -1,11 +1,21 @@
 import gmsh
 from pathlib import Path
-import os
 
 def empty_mesh(filename):
     gmsh.initialize()
     gmsh.model.add("simple")
-    Path(filename).parent.mkdir(parents=True, exist_ok=True)
+    gmsh.model.geo.addPoint(0, 0, 0, 1)
+    gmsh.model.geo.addPoint(1, 0, 0, 1)
+    gmsh.model.geo.addPoint(1, 1, 0, 1)
+    gmsh.model.geo.addPoint(0, 1, 0, 1)
+    gmsh.model.geo.addLine(1, 2, 1)
+    gmsh.model.geo.addLine(2, 3, 2)
+    gmsh.model.geo.addLine(3, 4, 3)
+    gmsh.model.geo.addLine(4, 1, 4)
+    gmsh.model.geo.addCurveLoop([1, 2, 3, 4], 1)
+    gmsh.model.geo.addPlaneSurface([1], 1)
+    gmsh.model.geo.synchronize()
+    gmsh.model.mesh.generate(2)
     gmsh.write(filename)
     gmsh.finalize()
 
