d23cf9a5ff9f4849484ccb6b59eba7b21422d3bd
RemDelaporteMathurin <rdelaportemathurin@gmail.com>
2024-10-28 13:28:39 -0400

    explicit paths only for meshio + seperated cells

--- case_1_simple/mesh.py
+++ case_1_simple/mesh.py
@@ -1,25 +1,6 @@
 import gmsh
 from pathlib import Path
 
-def empty_mesh(filename):
-    gmsh.initialize()
-    gmsh.model.add("simple")
-    gmsh.model.geo.addPoint(0, 0, 0, 1)
-    gmsh.model.geo.addPoint(1, 0, 0, 1)
-    gmsh.model.geo.addPoint(1, 1, 0, 1)
-    gmsh.model.geo.addPoint(0, 1, 0, 1)
-    gmsh.model.geo.addLine(1, 2, 1)
-    gmsh.model.geo.addLine(2, 3, 2)
-    gmsh.model.geo.addLine(3, 4, 3)
-    gmsh.model.geo.addLine(4, 1, 4)
-    gmsh.model.geo.addCurveLoop([1, 2, 3, 4], 1)
-    gmsh.model.geo.addPlaneSurface([1], 1)
-    gmsh.model.geo.synchronize()
-    gmsh.model.mesh.generate(2)
-    Path(filename).parent.mkdir(parents=True, exist_ok=True)
-    gmsh.write(filename)
-    gmsh.finalize()
-
 def three_cubes(filename, size=0.1):
     gmsh.initialize()
 
