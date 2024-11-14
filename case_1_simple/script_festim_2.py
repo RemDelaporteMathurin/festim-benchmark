ace6dad187f2d41af45b6defc650236f7b302ee3
RemDelaporteMathurin <rdelaportemathurin@gmail.com>
2024-11-14 09:24:49 -0500

    only 1 trap + make sure same results

--- case_1_simple/script_festim_2.py
+++ case_1_simple/script_festim_2.py
@@ -4,7 +4,7 @@ import festim as F
 # dolfinx.log.set_log_level(dolfinx.log.LogLevel.INFO)
 
 
-def run_festim_2(volume_file: str, facet_file: str):
+def run_festim_2_nietsche(volume_file: str, facet_file: str, exports=False):
     assert hasattr(
         F, "HydrogenTransportProblem"
     ), "you should use FESTIM on the fenicsx branch"
@@ -41,32 +41,21 @@ def run_festim_2(volume_file: str, facet_file: str):
     my_model.subdomains = [vol1, vol2, vol3, surface1, surface2]
 
     mobile = F.Species(name="H", subdomains=my_model.volume_subdomains, mobile=True)
-    trapped_1a = F.Species(name="H_trapped_W1", subdomains=[vol1], mobile=False)
-    trapped_1b = F.Species(name="H_trapped_W2", subdomains=[vol1], mobile=False)
+    trapped_1 = F.Species(name="H_trapped_W", subdomains=[vol1], mobile=False)
     trapped_2 = F.Species(name="H_trapped_cu", subdomains=[vol2], mobile=False)
-    empty_1a = F.ImplicitSpecies(n=0.5, others=[trapped_1a])
-    empty_1b = F.ImplicitSpecies(n=0.5, others=[trapped_1b])
+    empty_1 = F.ImplicitSpecies(n=0.5, others=[trapped_1])
     empty_2 = F.ImplicitSpecies(n=0.5, others=[trapped_2])
 
-    my_model.species = [mobile, trapped_1a, trapped_1b, trapped_2]
+    my_model.species = [mobile, trapped_1, trapped_2]
 
     my_model.reactions = [
         F.Reaction(
-            reactant=[mobile, empty_1a],
-            product=[trapped_1a],
+            reactant=[mobile, empty_1],
+            product=[trapped_1],
             k_0=1,
             E_k=tungsten.E_D,
             p_0=0.1,
             E_p=0.87,
-            volume=vol1,
-        ),
-        F.Reaction(
-            reactant=[mobile, empty_1b],
-            product=[trapped_1b],
-            k_0=1,
-            E_k=tungsten.E_D,
-            p_0=0.1,
-            E_p=1.0,
             volume=vol1,
         ),
         F.Reaction(
@@ -100,37 +89,34 @@ def run_festim_2(volume_file: str, facet_file: str):
     )
     my_model.settings.stepsize = F.Stepsize(1)
 
-    # my_model.exports = [
-    #     F.VTXSpeciesExport(
-    #         f"results_festim_2/mobile_{subdomain.id}.bp",
-    #         field=mobile,
-    #         subdomain=subdomain,
-    #     )
-    #     for subdomain in my_model.volume_subdomains
-    # ]
-    # [
-    #     F.VTXSpeciesExport(
-    #         f"results_festim_2/trapped_{vol1.id}a.bp",
-    #         field=trapped_1a,
-    #         subdomain=vol1,
-    #     ),
-    #     F.VTXSpeciesExport(
-    #         f"results_festim_2/trapped_{vol1.id}b.bp",
-    #         field=trapped_1b,
-    #         subdomain=vol1,
-    #     ),
-    #     F.VTXSpeciesExport(
-    #         f"results_festim_2/trapped_{vol2.id}.bp",
-    #         field=trapped_2,
-    #         subdomain=vol2,
-    #     ),
-    # ]
+    if exports:
+        folder = "results_festim_2/nietsche"
+        my_model.exports = [
+            F.VTXSpeciesExport(
+                f"{folder}/mobile_{subdomain.id}.bp",
+                field=mobile,
+                subdomain=subdomain,
+            )
+            for subdomain in my_model.volume_subdomains
+        ]
+        my_model.exports += [
+            F.VTXSpeciesExport(
+                f"{folder}/trapped_{vol1.id}a.bp",
+                field=trapped_1,
+                subdomain=vol1,
+            ),
+            F.VTXSpeciesExport(
+                f"{folder}/trapped_{vol2.id}.bp",
+                field=trapped_2,
+                subdomain=vol2,
+            ),
+        ]
 
     my_model.initialise()
     my_model.run()
 
 
-def run_festim_2_penalty(volume_file: str, facet_file: str):
+def run_festim_2_penalty(volume_file: str, facet_file: str, exports=False):
     assert hasattr(
         F, "HydrogenTransportProblem"
     ), "you should use FESTIM on the fenicsx branch"
@@ -159,40 +145,29 @@ def run_festim_2_penalty(volume_file: str, facet_file: str):
     vol3 = F.VolumeSubdomain(id=3, material=copper)
     surface1 = F.SurfaceSubdomain(id=4)
 
-    interface1 = F.Interface(id=6, subdomains=[vol1, vol2])
-    interface2 = F.Interface(id=7, subdomains=[vol2, vol3])
+    interface1 = F.Interface(id=6, subdomains=[vol1, vol2], penalty_term=500)
+    interface2 = F.Interface(id=7, subdomains=[vol2, vol3], penalty_term=500)
 
     surface2 = F.SurfaceSubdomain(id=5)
 
     my_model.subdomains = [vol1, vol2, vol3, surface1, surface2]
 
     mobile = F.Species(name="H", subdomains=my_model.volume_subdomains, mobile=True)
-    trapped_1a = F.Species(name="H_trapped_W1", subdomains=[vol1], mobile=False)
-    trapped_1b = F.Species(name="H_trapped_W2", subdomains=[vol1], mobile=False)
+    trapped_1 = F.Species(name="H_trapped_W", subdomains=[vol1], mobile=False)
     trapped_2 = F.Species(name="H_trapped_cu", subdomains=[vol2], mobile=False)
-    empty_1a = F.ImplicitSpecies(n=0.5, others=[trapped_1a])
-    empty_1b = F.ImplicitSpecies(n=0.5, others=[trapped_1b])
+    empty_1 = F.ImplicitSpecies(n=0.5, others=[trapped_1])
     empty_2 = F.ImplicitSpecies(n=0.5, others=[trapped_2])
 
-    my_model.species = [mobile, trapped_1a, trapped_1b, trapped_2]
+    my_model.species = [mobile, trapped_1, trapped_2]
 
     my_model.reactions = [
         F.Reaction(
-            reactant=[mobile, empty_1a],
-            product=[trapped_1a],
+            reactant=[mobile, empty_1],
+            product=[trapped_1],
             k_0=1,
             E_k=tungsten.E_D,
             p_0=0.1,
             E_p=0.87,
-            volume=vol1,
-        ),
-        F.Reaction(
-            reactant=[mobile, empty_1b],
-            product=[trapped_1b],
-            k_0=1,
-            E_k=tungsten.E_D,
-            p_0=0.1,
-            E_p=1.0,
             volume=vol1,
         ),
         F.Reaction(
@@ -226,37 +201,34 @@ def run_festim_2_penalty(volume_file: str, facet_file: str):
     )
     my_model.settings.stepsize = F.Stepsize(1)
 
-    # my_model.exports = [
-    #     F.VTXSpeciesExport(
-    #         f"results_festim_2/mobile_{subdomain.id}.bp",
-    #         field=mobile,
-    #         subdomain=subdomain,
-    #     )
-    #     for subdomain in my_model.volume_subdomains
-    # ]
-    # [
-    #     F.VTXSpeciesExport(
-    #         f"results_festim_2/trapped_{vol1.id}a.bp",
-    #         field=trapped_1a,
-    #         subdomain=vol1,
-    #     ),
-    #     F.VTXSpeciesExport(
-    #         f"results_festim_2/trapped_{vol1.id}b.bp",
-    #         field=trapped_1b,
-    #         subdomain=vol1,
-    #     ),
-    #     F.VTXSpeciesExport(
-    #         f"results_festim_2/trapped_{vol2.id}.bp",
-    #         field=trapped_2,
-    #         subdomain=vol2,
-    #     ),
-    # ]
+    if exports:
+        folder = "results_festim_2/penalty"
+        my_model.exports = [
+            F.VTXSpeciesExport(
+                f"{folder}/mobile_{subdomain.id}.bp",
+                field=mobile,
+                subdomain=subdomain,
+            )
+            for subdomain in my_model.volume_subdomains
+        ]
+        my_model.exports += [
+            F.VTXSpeciesExport(
+                f"{folder}/trapped_{vol1.id}a.bp",
+                field=trapped_1,
+                subdomain=vol1,
+            ),
+            F.VTXSpeciesExport(
+                f"{folder}/trapped_{vol2.id}.bp",
+                field=trapped_2,
+                subdomain=vol2,
+            ),
+        ]
 
     my_model.initialise()
     my_model.run()
 
 
-def run_festim_2_change_of_var(volume_file: str, facet_file: str):
+def run_festim_2_change_of_var(volume_file: str, facet_file: str, exports=False):
     my_model = F.HydrogenTransportProblemDiscontinuousChangeVar(
         petsc_options={
             "ksp_type": "preonly",
@@ -288,32 +260,21 @@ def run_festim_2_change_of_var(volume_file: str, facet_file: str):
     mobile = F.SpeciesChangeVar(
         name="H", subdomains=my_model.volume_subdomains, mobile=True
     )
-    trapped_1a = F.Species(name="H_trapped_W1", subdomains=[vol1], mobile=False)
-    trapped_1b = F.Species(name="H_trapped_W2", subdomains=[vol1], mobile=False)
+    trapped_1 = F.Species(name="H_trapped_W1", subdomains=[vol1], mobile=False)
     trapped_2 = F.Species(name="H_trapped_cu", subdomains=[vol2], mobile=False)
-    empty_1a = F.ImplicitSpecies(n=0.5, others=[trapped_1a])
-    empty_1b = F.ImplicitSpecies(n=0.5, others=[trapped_1b])
+    empty_1a = F.ImplicitSpecies(n=0.5, others=[trapped_1])
     empty_2 = F.ImplicitSpecies(n=0.5, others=[trapped_2])
 
-    my_model.species = [mobile, trapped_1a, trapped_1b, trapped_2]
+    my_model.species = [mobile, trapped_1, trapped_2]
 
     my_model.reactions = [
         F.Reaction(
             reactant=[mobile, empty_1a],
-            product=[trapped_1a],
+            product=[trapped_1],
             k_0=1,
             E_k=tungsten.E_D,
             p_0=0.1,
             E_p=0.87,
-            volume=vol1,
-        ),
-        F.Reaction(
-            reactant=[mobile, empty_1b],
-            product=[trapped_1b],
-            k_0=1,
-            E_k=tungsten.E_D,
-            p_0=0.1,
-            E_p=1.0,
             volume=vol1,
         ),
         F.Reaction(
@@ -342,29 +303,43 @@ def run_festim_2_change_of_var(volume_file: str, facet_file: str):
     )
     my_model.settings.stepsize = F.Stepsize(1)
 
-    # my_model.exports = [
-    #     F.VTXSpeciesExport(
-    #         f"results_festim_2/mobile_{subdomain.id}.bp", field=mobile, subdomain=subdomain
-    #     )
-    #     for subdomain in my_model.volume_subdomains
-    # ]
-    # [
-    #     F.VTXSpeciesExport(
-    #         f"results_festim_2/trapped_{vol1.id}a.bp",
-    #         field=trapped_1a,
-    #         subdomain=vol1,
-    #     ),
-    #     F.VTXSpeciesExport(
-    #         f"results_festim_2/trapped_{vol1.id}b.bp",
-    #         field=trapped_1b,
-    #         subdomain=vol1,
-    #     ),
-    #     F.VTXSpeciesExport(
-    #         f"results_festim_2/trapped_{vol2.id}.bp",
-    #         field=trapped_2,
-    #         subdomain=vol2,
-    #     ),
-    # ]
+    if exports:
+        folder = "results_festim_2/change_of_var"
+        my_model.exports = [
+            F.VTXSpeciesExport(
+                f"{folder}/mobile.bp",
+                field=mobile,
+            ),
+            F.VTXSpeciesExport(
+                f"{folder}/trapped_1.bp",
+                field=trapped_1,
+            ),
+            F.VTXSpeciesExport(
+                f"{folder}/trapped_2.bp",
+                field=trapped_2,
+            ),
+        ]
 
     my_model.initialise()
     my_model.run()
+
+
+if __name__ == "__main__":
+    from mesh import three_cubes
+
+    size = 0.05
+    filename = f"meshes/mesh_{size}.msh"
+
+    three_cubes(filename, size=size)
+
+    from convert_mesh import convert_mesh
+
+    volume_file = f"meshes/mesh_{size}.xdmf"
+    facet_file = f"meshes/mesh_{size}_facet.xdmf"
+
+    print(filename, volume_file, facet_file)
+    nb_cells, nb_facets = convert_mesh(filename, volume_file, facet_file)
+
+    run_festim_2_nietsche(volume_file, facet_file, exports=True)
+    run_festim_2_penalty(volume_file, facet_file, exports=True)
+    run_festim_2_change_of_var(volume_file, facet_file, exports=True)
