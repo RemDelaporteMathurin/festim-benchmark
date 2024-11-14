2b8964f147af8b15d407fff3b7db3d13fbaa3558
Jorgen Dokken <dokken92@gmail.com>
2024-11-14 21:35:58 +0000

    add new plotting script

---
+++ case_1_simple/plotting.py
@@ -0,0 +1,124 @@
+import pandas
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def plot_runtime(
+    num_procs: np.ndarray,
+    runtimes_mixed: np.ndarray = None,
+    runtimes_change_of_var: np.ndarray = None,
+    runtimes_penalty: np.ndarray = None,
+    show_festim_1: bool = True,
+    show_ideal: bool = True,
+):
+    lines = []
+    if runtimes_mixed is not None:
+        (l_mixed,) = plt.plot(
+            num_procs, runtimes_mixed, marker="*", label=f"mixed domain", color="navy"
+        )
+        lines.append(l_mixed)
+        if show_ideal:
+            n = np.linspace(1, 16, 100)
+            (l_ideal,) = plt.plot(
+                n, runtimes_mixed[0] * n**-1, linestyle="--", color="tab:grey"
+            )
+            # lines.append(l_ideal)
+    if runtimes_penalty is not None:
+        # another green
+        (l_penalty,) = plt.plot(
+            num_procs, runtimes_penalty, marker="s", label="penalty", color="firebrick"
+        )
+        lines.append(l_penalty)
+        if show_ideal:
+            n = np.linspace(1, 16, 100)
+            (l_ideal,) = plt.plot(
+                n, runtimes_penalty[0] * n**-1, linestyle="--", color="tab:grey"
+            )
+            # lines.append(l_ideal)
+    if runtimes_change_of_var is not None:
+        (l_change_var,) = plt.plot(
+            num_procs,
+            runtimes_change_of_var,
+            marker="o",
+            label="change of variable",
+            color="forestgreen",
+        )
+        lines.append(l_change_var)
+        if show_ideal:
+            n = np.linspace(1, 16, 100)
+            (l_ideal,) = plt.plot(
+                n,
+                runtimes_change_of_var[0] * n**-1,
+                label="ideal",
+                linestyle="--",
+                color="tab:grey",
+            )
+            lines.append(l_ideal)
+
+    if show_festim_1:
+        # check that runtimes_festim_1.csv exists and plot the results
+        try:
+            import pandas as pd
+
+            data = pd.read_csv("runtimes_festim_1.csv")
+            (l_festim1,) = plt.plot(
+                data["num_procs"],
+                data["runtimes"],
+                marker="o",
+                label="FESTIM v1.3.1",
+                color="tab:red",
+            )
+            lines.append(l_festim1)
+        except FileNotFoundError:
+            print("File runtimes_festim_1.csv not found")
+
+    for line in lines:
+        plt.annotate(
+            f"{line.get_label()}",
+            (num_procs[-1], line.get_ydata()[-1]),
+            textcoords="offset points",
+            xytext=(5, 0),
+            ha="left",
+            va="center",
+            color=line.get_color(),
+        )
+
+    if runtimes_mixed  is not None or runtimes_change_of_var is not None or penalty is not None:
+        plt.annotate(
+            "FESTIM 2",
+            (num_procs[2], runtimes_change_of_var[2]),
+            textcoords="offset points",
+            xytext=(5, 30),
+            ha="left",
+            va="center",
+            color="tab:green",
+        )
+
+    # plt.legend()
+    plt.xlabel("Number of processes")
+    plt.ylabel("Runtime (s)")
+
+    plt.xscale("log")
+    plt.yscale("log")
+
+    # xticks only at the powers of 2
+    plt.xticks(num_procs, num_procs)
+
+    # remove top and right spines
+    plt.gca().spines["top"].set_visible(False)
+    plt.gca().spines["right"].set_visible(False)
+    plt.legend()
+
+tab = pandas.read_csv("results.csv", sep=",")
+filtered_tab = tab[tab["MeshSize"]==0.05]
+procs = filtered_tab["NumProcs"]
+sort = np.argsort(procs)
+
+mixed = filtered_tab["Mixed"][sort].array
+change = filtered_tab["ChangeVar"][sort].array
+penalty = filtered_tab["Penalty"][sort].array
+num_procs = procs[sort].array
+plot_runtime(num_procs,mixed, change, penalty, show_festim_1=False, show_ideal=True)
+# ylim = plt.gca().get_ylim()
+# xlim = plt.gca().get_xlim()
+plt.savefig("all.png")
